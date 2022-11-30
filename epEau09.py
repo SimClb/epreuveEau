102644690512f5f8e65b6457feb54ead8710be0c
SCL <76017102+SimClb@users.noreply.github.com>
2022-11-30 01:39:34 +0100

    exo9 (between min and max)

--- epEau09.py
+++ epEau09.py
@@ -2,27 +2,54 @@
 import sys
 import OurLib
 
+# data preparation
+
+list = sys.argv[1:]
+secondList = []
+for i in list:
+    secondList.append(int(i))
 
 
 # function 
 
+def includedPrinter():
 
+    if secondList[0] < secondList[1]:
+        rangeIndex = secondList[1] - secondList[0]
+        starting = secondList[0]
+    else:
+        rangeIndex = secondList[0] - secondList[1]
+        starting = secondList[1]
 
+    output = ""
 
-# error management
+    for m in range(rangeIndex):
+        output += f'{starting}' + ' '
+        starting += 1
+
+    print(output)
+    
+
+# error management 
 
 if OurLib.lenCounter(sys.argv) != 3:
     print('error')
     exit()
 
 for i in sys.argv[1:]:
-    isNumber = OurLib.isNumericHomeMade
+    isNumber = OurLib.isNumericHomeMade(i)
+    if isNumber == False:
+        print('error')
+        exit()
 
+if secondList[0] == secondList[1] or secondList[0] == secondList[1] - 1 or secondList[0] == secondList[1] + 1:
+    print('error')
+    exit()
 
 ################################################################
 
-
-
+includedPrinter()
+#finished 
 
 # pseudo code: 
 
--- test.py
+++ test.py
@@ -1,22 +1,3 @@
 import time 
 import sys
 
-variable = "Simon"
-
-def len(var): 
-    count = 0
-    for i in var:
-        count += 1
-
-    print(count)
-    return count
-
-    len()
-    
-
-
-
-
-
-print(len(variable))
-
